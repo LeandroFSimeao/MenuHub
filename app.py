d91b1d049414c1139bb10598b26d4fc99de769af
Leandro <leandro.ferreira@brunsker.com.br>
2023-01-17 12:42:05 -0300

    Ponto

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # required packages.
 import os
-
 from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -11,17 +10,17 @@ from flask_migrate import Migrate
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+Bootstrap(app)
+
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = f"sqlite:///{os.path.join(basedir, 'menuhub.sqlite')}"
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 
-Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 # class models.
-
 
 class Dish(db.Model):
 
@@ -33,15 +32,17 @@ class Dish(db.Model):
     price = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
-        return f"Task: #{self.id}, description: {self.description}"
+        return f"Dish: #{self.id}, name: {self.name}"
 
 # applications routes.
 
 
-@app.route('/', methods=["POST", "GET"])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == "POST":
-        dish = Dish(request.form["name"])
+    if request.method == 'POST':
+        dish = Dish(name=request.form['name'].upper(),
+                    category=request.form['category'],
+                    price=float(request.form['price']))
         try:
             db.session.add(dish)
             db.session.commit()
@@ -52,12 +53,28 @@ def index():
         dishes = Dish.query.order_by(Dish.price).all()
         return render_template('index.html', dishes=dishes)
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    """delete a dish"""
+    dish = Dish.query.get_or_404(id)
+    try:
+        db.session.delete(dish)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Houve um erro, ao inserir o prato"
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
 
-
-@app.route('/user/')
-def test():
-    return render_template('user.html')
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    """update route"""
+    dish = Dish.query.get_or_404(id)
+    if request.method == 'POST':
+        dish.name = request.form['name']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Houve um erro, ao atualizar o prato"
+    else:
+        return render_template('update.html', dish=dish)
